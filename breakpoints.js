5f0adfb70452877353ea33a1ccb811586e28f2ad
Eike Send <eike.send@gmail.com>
2012-03-29 17:47:52 +0200

    removing jQuery dependency, can be used standalone now

--- breakpoints.js
+++ breakpoints.js
@@ -1,22 +1,96 @@
 /*
-	Breakpoints.js
-	version 2.0
-	
-	Creates handy events for your responsive design breakpoints
-	
-	Copyright 2011-2012 XOXCO, Inc
-	http://xoxco.com/
+ * Breakpoints.js
+ * Version 3.0
+ * 
+ * Creates handy events for your responsive design breakpoints.
+ * Use it like this to bind to certain breakpoint changes:
+ * 
+ * breakpoints([0, 480, 960], function(oldP, newP) { 
+ *   console.log(oldP, newP)
+ * });
+ * 
+ * Version 1.0 written and copyright by XOXCO, Inc
+ * http://xoxco.com/
+ * 
+ * Version 2.0 and 3.0 rewrite and copyright by Eike Send
+ * http://eike.se/nd
+ * 
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
 
-	Version 2.0 rewrite by Eike Send
-	http://eike.se/nd
-
-	Licensed under the MIT license:
-	http://www.opensource.org/licenses/mit-license.php
-
-*/
-(function($) {
+(function(){
+  var oldBP = currentBP = -1,
+      breakpointArray,
+      lastCall = 0,
+      resizeDefer, 
+      resizeThrottle = 30;
   
-  $.getCurrentBreakPoint = function(breakpoints, width) {
+  // This is the function that is exported into the global namespace
+  breakpoints = function(userBreakpoints, callback) {
+    // just in case, sort the given breakpoints
+    breakpointArray = userBreakpoints.sort(function(a, b) { return a - b } );
+    // Bind to the resize event, but don't remove other bindings
+    var oldResizeFunc = window.onresize;
+    window.onresize = function(){
+      if (oldResizeFunc) {
+        oldResizeFunc();
+      }
+      checkBreakPoints(true);
+    }
+    if (callback) {
+      breakpoints.bind(callback);
+    }
+    checkBreakPoints();
+  }
+  
+  // Does the actual work:
+  var checkBreakPoints = function(fromResize) {
+    
+    // Throttle the calls to this function but still allow immediate reaction
+    var now = (new Date()).getTime();
+    if( fromResize && lastCall && now - lastCall < resizeThrottle ){
+      clearTimeout( resizeDefer );
+      resizeDefer = setTimeout( checkBreakPoints, resizeThrottle );
+      return;
+    }
+    else {
+      lastCall = now;
+    }
+    
+    // Get window width, code stolen from jQuery
+    var docwindowProp = document.documentElement["clientWidth"];
+    var width = document.compatMode === "CSS1Compat" && docwindowProp 
+                || document.body && document.body["clientWidth"] 
+                || docwindowProp;
+    
+    // This checks if breakpoints have changed and triggers accordingly
+    currentBP = breakpoints.getCurrentBreakPoint(breakpointArray, width);
+    if (oldBP != currentBP) {
+      var inBetweenBreakPoints = [];
+      // Find all the breakpoints that have been crossed and trigger the event as well
+      for (var i = 0; i < breakpointArray.length; i++) {
+        var el = breakpointArray[i]; 
+        if (el >= oldBP && el <= currentBP || el >= currentBP && el <= oldBP) {
+          inBetweenBreakPoints.push(el);
+        }
+      }
+      if (currentBP < oldBP) {
+        inBetweenBreakPoints = inBetweenBreakPoints.reverse();
+      }
+      for (var i = 0; i < inBetweenBreakPoints.length; i++) {
+        trigger(inBetweenBreakPoints[i]);
+      }
+      oldBP = currentBP;
+    }
+  }
+  
+  // Utility function to get the value that is the highest in an array below or equal the given value
+  // Defaults to 0
+  // breakpoints.getCurrentBreakPoint([100, 200, 300, 400], 250) -> 200
+  // breakpoints.getCurrentBreakPoint([100, 200, 300, 400], 25)  -> 0
+  breakpoints.getCurrentBreakPoint = function(breakpoints, width) {
     for (var bp in breakpoints) {
       bp = parseInt(bp);
       if (width >= breakpoints[bp] && ( breakpoints.length == bp + 1 || width < breakpoints[bp+1] )) {
@@ -26,61 +100,25 @@
     return 0;
   };
   
-  $.fn.breakpoints = function(breakpoints) {
-    
-    var oldBP = currentBP = -1; 
-    breakpoints = breakpoints.sort(function(a,b){return a-b});
-    
-    var lastCall = 0,
-      resizeDefer, 
-      resizeThrottle = 30;
-    
-    var checkBreakPoints = function(fromResize) {
-      
-      // This throttles the calls to this function and still allows immediate reaction
-      var now = (new Date()).getTime();
-      if( fromResize && lastCall && now - lastCall < resizeThrottle ){
-        clearTimeout( resizeDefer );
-        resizeDefer = setTimeout( checkBreakPoints, resizeThrottle );
-        return;
-      }
-      else {
-        lastCall = now;
-      }
-      
-      // This checks if breakpoints have changed and triggers accordingly
-      var width = $(window).width();
-      currentBP = $.getCurrentBreakPoint(breakpoints, width);
-      if (oldBP != currentBP) {
-        var inBetweenBreakPoints = $.map(breakpoints, function(el) {
-          if (el >= oldBP && el <= currentBP || el >= currentBP && el <= oldBP) {
-            return el;
-          }
-          return null
-        });
-        if (currentBP < oldBP) {
-          inBetweenBreakPoints = inBetweenBreakPoints.reverse();
-        }
-        if (inBetweenBreakPoints.length > 1) { 
-          for (var i = 0; i < inBetweenBreakPoints.length-1; i++ ) {
-            var bp1 = inBetweenBreakPoints[i]
-                bp2 = inBetweenBreakPoints[i+1];
-            $(window).trigger('changeBreakpoint', {oldBP: bp1, newBP: bp2 });
-            $(window).trigger('changeBreakpoint-' + bp1 + "-" + bp2);
-          }
-        } else {
-          $(window).trigger('changeBreakpoint', {oldBP: 0, newBP: currentBP });
-          $(window).trigger('changeBreakpoint-' + 0 + "-" + currentBP);
-        }
-        oldBP = currentBP;
+  var breakpointsHistory = [], boundFunctions = [];
+  // These functions are are used to bind and trigger callbacks of breakpoint events
+  breakpoints.bind = function(func) {
+    boundFunctions.push(func);
+    if (breakpointsHistory.length) {
+      for (var i = 1; i < breakpointsHistory.length; i++) {
+        func(breakpointsHistory[i-1], breakpointsHistory[i])
       }
     }
-
-    function resizeBreakPoints(){
-      checkBreakPoints(true);
+  }
+  
+  var trigger = function(value) {
+    for (var i = 0; i < boundFunctions.length; i++) {
+      var func = boundFunctions[i];
+      if (breakpointsHistory.length > 0 
+          && breakpointsHistory[breakpointsHistory.length-1] != value) {
+        func(breakpointsHistory[breakpointsHistory.length-1], value)
+      }
     }
-
-    $(window).bind("resize", resizeBreakPoints).resize();
-    
-  };
-})(jQuery);
+    breakpointsHistory.push(value);
+  }
+})();
